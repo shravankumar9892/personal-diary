70eae6beffcd12639b5423b002d8d3d130ede33c
Shetty <shravashetty@deloitte.com>
2021-12-14 23:16:15 +0530

    using query service to fetch data

--- src/PostList.js
+++ src/PostList.js
@@ -7,7 +7,7 @@ export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    const res = await axios.get("http://localhost:4002/posts");
     setPosts(res.data);
   };
 
@@ -15,21 +15,24 @@ export default () => {
     fetchPosts();
   }, []);
 
-  const renderedPosts = Object.values(posts).map((post) => {
-    return (
-      <div
-        className="card"
-        style={{ width: "30%", marginBottom: "20px" }}
-        key={post.id}
-      >
-        <div className="card-body">
-          <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
-          <CommentCreate postId={post.id} />
+  const renderedPosts = Object.values(posts)
+    .slice(0)
+    .reverse()
+    .map((post) => {
+      return (
+        <div
+          className="card"
+          style={{ width: "30%", marginBottom: "20px" }}
+          key={post.id}
+        >
+          <div className="card-body">
+            <h3>{post.title}</h3>
+            <CommentList comments={post.comments} />
+            <CommentCreate postId={post.id} />
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
   return (
     <div className="d-flex flex-row flex-wrap justify-content-between">
       {renderedPosts}
